6db3c66ae03a171bd3d0a9d2525c93d943df2db2
zain2236 <syedzain2236@gmail.com>
2026-02-15 14:11:16 +0500

    added socail links

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,11 +1,11 @@
-// import { vercelPreset } from "@vercel/react-router/vite";
+import { vercelPreset } from "@vercel/react-router/vite";
 import type { Config } from "@react-router/dev/config";
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-  // presets: [vercelPreset()],
+  presets: [vercelPreset()],
   // Pre-render static pages at build time for better performance and SEO
   async prerender() {
     return [
